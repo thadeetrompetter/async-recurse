6ee117e3f5e5f6018dbb4b71920ffa3855335f83
Thadee Trompetter <thadee@voorhoede.nl>
2015-10-18 10:09:58 +0200

    refactor client, add todos

--- index.js
+++ index.js
@@ -1,38 +1,46 @@
 var http = require('http'),
     store = require('./store'),
-    location = 'http://localhost:3000';
+    location = 'http://localhost:3000',
+    randomDate = require('./random-date');
 
-function getItems(location, store) {
+function getItems(store, iterator, errorCallback) {
     var stash = store.slice(),
-        conversations = extractConversations(stash),
-        cache = {},
-        count = 0;
-        function iterateConversations(history, success, error) {
-            conversations.forEach(function (conversation, index, array) {
-                history.forEach(function (historyItem) {
-                    if(historyItem.key === conversation.channel){
-                        conversation.message = historyItem.message;
-                        count++;
-                    }
-                });
-                if(count === conversations.length){
-                    return process.nextTick(function () {
-                        success(conversations)
-                        process.exit();
-                    });
-                }
-                return requestData(function (response) {
-                    return process.nextTick(function () {
-                        iterateConversations(response, success, error)
-                    });
-                }, error);
-            });
+        extractConversations = extractFromEach(stash, 'conversations'),
+        conversations = extractConversations();
+
+    requestData(function (history) {
+        // TODO: in the pubnub history call, iterator needs to be configured to
+        // only accept history as argument.
+        iterator(conversations, history);
+    }, errorCallback);
+}
+// TODO: add option to break out of conversations loop if as many results were
+// collected as there are items in retrieved history.
+function iteratorFactory(success, error) {
+    var count = 0;
+    return function iterateConversations(conversations, history) {
+        var historyByChannel = getHashByPropertyValue('key', history),
+            hits = 0;
+        conversations.forEach(function (conversation, index) {
+            var channel = conversation.channel;
+            if(historyByChannel.hasOwnProperty(channel)){
+                conversation.timestamp = historyByChannel[channel].timestamp;
+                count++;
+                hits++;
+            }
+            if(hits === history.length){
+                // this does not work in a foreach, re-write to for loop
+                // to use 'break'
+                return false;
+            }
+        });
+        if(count === conversations.length){
+            return success(conversations);
         }
-        return function (success, error) {
-            requestData(function (response) {
-                iterateConversations(response, success, error);
-            }, error);
-        };
+        requestData(function (history) {
+            iterateConversations(conversations, history);
+        }, error);
+    };
 }
 function requestData(successCallback, errorCallback) {
     return http.get({
@@ -48,37 +56,49 @@ function requestData(successCallback, errorCallback) {
         res.on('error', errorCallback);
     });
 }
-// requestData(function (response) {
-//
-// });
+function getHashByPropertyValue (property, collection) {
+    var len = collection.length,
+        i = 0,
+        collector = {};
 
-function storeHistory (items, cache) {
-    var len = items.length,
-        i = 0;
-
-        while(i < l){
-            if(!cache.hasOwnProperty(item)){
-                cache[item] = items[item];
-            }
-            i++;
-        }
-}
-function extractConversations(stash) {
-    var conversations = [],
-        len = stash.length,
-        i = 0;
     while(i < len){
-        if(stash[i].hasOwnProperty('conversations')){
-            conversations = conversations.slice().concat(stash[i].conversations);
+        var current;
+        if(collection[i].hasOwnProperty(property)){
+            current = collector[collection[i][property]] = {};
+            for(var prop in collection[i]){
+                if(prop !== property){
+                    current[prop] = collection[i][prop];
+                }
+            }
         }
         i++;
     }
-    return conversations;
+    return collector;
 }
-var retriever = getItems(location, store);
+exports.getHashByPropertyValue = getHashByPropertyValue;
 
-retriever(function (res) {
+function extractFromEach(collection, key) {
+    return function () {
+        var collector = [],
+            len = collection.length,
+            i = 0;
+        while(i < len){
+            if(collection[i].hasOwnProperty(key)){
+                collector = collector.concat(collection[i][key]);
+            }
+            i++;
+        }
+        return collector;
+    };
+}
+exports.extractFromEach = extractFromEach;
+
+var iterator = iteratorFactory(function (res) {
     console.log(res);
 }, function (err) {
     console.log(err);
+});
+
+getItems(store, iterator, function () {
+    console.log('bad shit');
 });
